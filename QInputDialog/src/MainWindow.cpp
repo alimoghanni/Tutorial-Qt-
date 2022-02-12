96a03bbf09d8a05af0fbc87c55ccf87d0420bdc4
Ali Moghanni <a.moghanni>
2022-02-12 11:08:12 +0100

    added project QInputDialog

---
+++ QInputDialog/src/MainWindow.cpp
@@ -0,0 +1,25 @@
+#include "MainWindow.h"
+#include "ui_MainWindow.h"
+#include <QInputDialog>
+
+
+MainWindow::MainWindow(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    ui->label->setText("XXXX");
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_pushButton_clicked()
+{
+    double n = QInputDialog::getDouble(this, "Enter", "Input a value:", 0.00, -2147483647, 2147483647, 2);
+    ui->label->setText(QString::number(n));
+}
+
