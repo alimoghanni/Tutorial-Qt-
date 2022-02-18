9dfc5b6c033c6c363e1fd1bb5af5e88202716cb3
Ali Moghanni <a.moghanni@gmail.com>
2022-02-18 09:58:50 +0100

    added QListWidget project

---
+++ QListWidget/src/Widget.cpp
@@ -0,0 +1,31 @@
+#include "Widget.h"
+#include "ui_Widget.h"
+
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
+
+void Widget::on_btnAdd_clicked()
+{
+    QString name;
+    name = ui->lineEdit->text();
+    if (name.isEmpty())
+        return;
+    ui->listWidget->addItem(name);
+}
+
+
+void Widget::on_pushButton_2_clicked()
+{
+    delete ui->listWidget->currentItem();
+}
+
