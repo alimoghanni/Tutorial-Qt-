bc4ec57b6fe6b7dd311ca8470dcaf4fa9b21ccb7
Ali Moghanni <a.moghanni@gmail.com>
2022-02-22 11:25:36 +0100

    added QDir project

---
+++ QDir/src/main.cpp
@@ -0,0 +1,22 @@
+#include <QCoreApplication>
+#include <QDebug>
+#include <QDir>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    QDir directory;
+    directory.setPath("E:/Apps/Alfatraining_C++_Qt/Tutorial Qt Creator");
+    qDebug() << directory[3];
+
+    int total = directory.count();
+
+    for (int n = 0; n < total; ++n) {
+        qDebug() << directory[n];
+    }
+
+    qDebug() << directory.dirName();
+
+    return a.exec();
+}
