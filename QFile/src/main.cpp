673ef01e372dede2fc44de71420ab23cd2d8e2a2
Ali Moghanni <a.moghanni@gmail.com>
2022-02-23 09:54:49 +0100

    added QFile project

---
+++ QFile/src/main.cpp
@@ -0,0 +1,38 @@
+#include <QCoreApplication>
+#include <QDebug>
+#include <QFile>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    QByteArray content;
+    QFile qFile;
+    qFile.setFileName("E:/Apps/Alfatraining_C++_Qt/Tutorial Qt Creator/Tutorial Qt Creator - QFile/QFile/text.txt");
+
+    if (!qFile.exists())
+    {
+        qDebug() << "The file does not exists!";
+        return 1;
+    }
+
+    qFile.open(QIODevice::ReadOnly | QIODevice::Text);
+
+    if (!qFile.isOpen())
+    {
+        qDebug() << "The file does not open!";
+        return 2;
+    }
+
+    content = qFile.readAll();
+    qDebug() << content;
+
+    //content = "I am writing to file";
+    //qFile.open(QIODevice::WriteOnly | QIODevice::Text);
+    //qFile.write(content);
+    //qFile.flush();
+    //qFile.close();
+
+
+    return a.exec();
+}
