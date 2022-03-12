9845444162540a5c8a8a1a39905b1496e308fc48
Ali Moghanni <a.moghanni@gmail.com>
2022-03-12 13:54:18 +0100

    updated QVector project

--- QVector/src/main.cpp
+++ QVector/src/main.cpp
@@ -10,6 +10,13 @@ int main(int argc, char *argv[])
     vecInt << 1 << 2 << 7 << 9 << 5;
 
     qDebug() << vecInt;
+	
+	
+	QVector<double> vecDouble;
+    vecDouble << 1.2 << 2.9 << 7.3 << 9.0 << 5.5;
+
+    qDebug() << vecDouble;
+
 
     QVector<QString> vecQString;
     vecQString << "This" << "is" << "a" << "Vector" << "QString";
